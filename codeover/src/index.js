3e94d0b6807114c2cda29a8cea1313e66f693a88
Kevin DJEDJE <kevin.djedje@epitech.eu>
2018-03-23 09:26:16 +0100

    Ajout du prettier et de ESLint

--- codeover/src/index.js
+++ codeover/src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import Login from "./Login";
 
-ReactDOM.render(<App name="Cedric l'ivoirien"/>, document.getElementById('root'));
+ReactDOM.render(<App name="Cedric" />, document.getElementById("root"));
 registerServiceWorker();

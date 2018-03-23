7b485d67308efce50fa86789a864f3cf4f7495ff
voinne_c <cedric.voinnet@epitech.eu>
2018-03-23 10:34:17 +0100

    Routing sur les login, register et home

--- codeover/src/index.js
+++ codeover/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import Login from "./Login";
 
 ReactDOM.render(<App name="Cedric" />, document.getElementById("root"));
 registerServiceWorker();
